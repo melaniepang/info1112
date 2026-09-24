49840674c1f9f1d5b0eba4f027675ea11fcc4346
Melanie Pang <melaniewenting04@gmail.com>
2026-09-24 21:38:13 +1000

    Check .vsc extension

--- assembler.sh
+++ assembler.sh
@@ -9,3 +9,8 @@ if [[ ! -f "$1" ]]; then
     echo "Error: file '$1' does not exist"
     exit 1
 fi
+
+if [[ "$1" != *.vsc  ]]; then
+    echo "Error: file must have a .vsc extension"
+    exit 1
+fi
